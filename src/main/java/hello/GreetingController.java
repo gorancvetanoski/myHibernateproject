a142e03a32728740d35b433c36d4665836950955
gorancve <goran.cvetanoski@gmail.com>
2018-01-22 13:58:14 +0100

    commit all

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,7 +1,11 @@
 package hello;
 
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,11 +15,13 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@AuthenticationPrincipal final UserDetails userDetail, @RequestParam(value="name", defaultValue="World") String name) {
+       /* String userName = userDetail.getUsername();
+        Collection<? extends GrantedAuthority> authorities = userDetail.getAuthorities();
+        authorities.forEach(System.out::println);*/
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
-
 
     @RequestMapping(value = "/addgreeting", method = RequestMethod.PUT)
     public String addGreeting(@RequestBody Greeting name) {
